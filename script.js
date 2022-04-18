8260f439dc4bc273750b31c07c5a084c009da047
PratikshaSurwade <89350917+PratikshaSurwade@users.noreply.github.com>
2022-04-18 22:59:28 +0530

    song title on playboard

--- script.js
+++ script.js
@@ -29,7 +29,8 @@ function loadSong(songIndex) {
   // }
   // song=songIndex;
   console.log(songIndex);
-    title.innerText = document.getElementById(songIndex).parentNode.parentElement.childNodes;
+    title.innerText = document.getElementById(songIndex).parentNode.parentNode.childNodes[1].outerText;
+    console.log(document.getElementById(songIndex).parentNode.parentNode.childNodes[1].outerText);
     audio.src = `music/${songIndex}.mp3`;
     cover.src = `images/${songIndex}.jpg`;
     document.getElementById(songIndex).classList.toggle('fa-play-circle');
